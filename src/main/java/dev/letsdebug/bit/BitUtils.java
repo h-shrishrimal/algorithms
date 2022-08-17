dd5a23f241e9aceed2730acaa01f9b9334f1964e
Harshad S <hshrishrimal@gmail.com>
2022-08-17 14:39:32 +0530

    bit manipulation.

--- src/main/java/dev/letsdebug/bit/BitUtils.java
+++ src/main/java/dev/letsdebug/bit/BitUtils.java
@@ -19,7 +19,18 @@ public final class BitUtils {
   }
 
   /**
-   * get the {@code i } bit of {@code num}
+   * Check if {@code num} is power of 2
+   *
+   * @param num input to check
+   * @return {@code true} if power of 2, {@code false} otherwise
+   */
+  public static boolean isPowerOf2(long num) {
+    // if num is power of 2 its bit pattern will always be 1 followed by 0's
+    return (num & (num - 1)) == 0;
+  }
+
+  /**
+   * get the {@code i} bit of {@code num}
    *
    * @param num input
    * @param i bit to return
@@ -32,7 +43,7 @@ public final class BitUtils {
   }
 
   /**
-   * set the {@code i } bit of {@code num}
+   * set the {@code i} bit of {@code num}
    *
    * @param num input
    * @param i bit to set
@@ -45,11 +56,11 @@ public final class BitUtils {
   }
 
   /**
-   * clear the {@code i } bit in {@code num}
+   * clear the {@code i} bit in {@code num}
    *
-   * @param num
-   * @param i
-   * @return
+   * @param num input
+   * @param i bit to clear
+   * @return number with ith bit cleared
    */
   public static int clearIthBit(int num, int i) {
     // set all bits to 0 expect i bit and then invert all the bits
@@ -57,12 +68,62 @@ public final class BitUtils {
     return (num & mask);
   }
 
-public static int updateIthBit(int num, int i, int bit) {
-  // Clear the bit at the given position and add the resultant bit to the result.
-  // Create a mask and perform an AND operation with the result.
-  // Perform Binary OR operation with the mask we have created and perform the right shift
-  // operation with the value to be updated at the index.
-  int mask = clearIthBit(num, i);
-  return mask | (bit << i);
-}
+  /**
+   * update the {@code i} bit in {@code num} with {@code bit}
+   *
+   * @param num input
+   * @param i bit to update
+   * @param bit new update bit (0 or 1)
+   * @return number with {@code i} bit updated to {@code bit}
+   */
+  public static int updateIthBit(int num, int i, int bit) {
+    // Clear the bit at the given position and add the resultant bit to the result.
+    // Create a mask and perform an AND operation with the result.
+    // Perform Binary OR operation with the mask we have created and perform the right shift
+    // operation with the value to be updated at the index.
+    int mask = clearIthBit(num, i);
+    return mask | (bit << i);
+  }
+
+  /**
+   * clear last {@code i} bits
+   *
+   * @param num input
+   * @param i bits to start clearing from
+   * @return new number with {@code i} bits cleared
+   */
+  public static int clearLastIbits(int num, int i) {
+    int mask = (~0) << i;
+    return (num & mask);
+  }
+
+  /**
+   * clear bits in range {@code i} to {@code j}
+   *
+   * @param num input
+   * @param i start range index
+   * @param j end range index
+   * @return number with bits {@code i} to {@code j} cleared
+   */
+  public static int clearBitsInRange(int num, int i, int j) {
+    int mask_a = -1 << (j + 1);
+    int mask_b = (1 << i) - 1;
+    int mask = mask_a | mask_b;
+    return num & mask;
+  }
+
+  /**
+   * replace bits in range {@code i} to {@code j}
+   *
+   * @param num input
+   * @param i start range index
+   * @param j end range index
+   * @param val new bits to replace
+   * @return num with bits replaced with
+   */
+  public static int replaceBitsInRange(int num, int i, int j, int val) {
+    int n = clearBitsInRange(num, i, j);
+    int mask = val << i;
+    return n | mask;
+  }
 }
--- src/test/java/dev/letsdebug/sort/bit/BitUtilsTest.java
+++ src/test/java/dev/letsdebug/sort/bit/BitUtilsTest.java
@@ -10,6 +10,31 @@ public class BitUtilsTest {
 
   @ParameterizedTest(name = "input: {0}, expected: {1}")
   @CsvSource({
+    "2,true",
+    "3,false",
+    "4,true",
+    "8,true",
+    "16,true",
+    "32,true",
+    "64,true",
+    "128,true",
+    "256,true",
+    "512,true",
+    "1023,false",
+    "1024,true",
+    "1025,false",
+    "2048,true",
+    "4096,true",
+    "8192,true",
+    "1_125_899_906_842_624,true"
+  })
+  void isPowerOf2Test(long input, boolean expected) {
+    boolean actual = BitUtils.isPowerOf2(input);
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @ParameterizedTest(name = "input: {0}, expected: {1}")
+  @CsvSource({
     "0,false",
     "1,true",
     "2,false",
@@ -52,19 +77,48 @@ public class BitUtilsTest {
     Assertions.assertEquals(expectedInBinary, Integer.toBinaryString(actual));
   }
 
-  @ParameterizedTest(name = "input: {0}, bit: {1}")
+  @ParameterizedTest(name = "input: {0}, i: {1}")
   @CsvSource({
-          "10,0,1010",
-          "10,1,1000",
-          "10,2,1010",
-          "10,3,10",
-          "-10,0,11111111111111111111111111110110",
-          "-10,1,11111111111111111111111111110100",
-          "-10,2,11111111111111111111111111110010",
-          "-10,3,11111111111111111111111111110110"
+    "10,0,1010",
+    "10,1,1000",
+    "10,2,1010",
+    "10,3,10",
+    "-10,0,11111111111111111111111111110110",
+    "-10,1,11111111111111111111111111110100",
+    "-10,2,11111111111111111111111111110010",
+    "-10,3,11111111111111111111111111110110"
   })
   void clearIthBitTest(int num, int i, String expectedInBinary) {
     int actual = BitUtils.clearIthBit(num, i);
+    Assertions.assertEquals(expectedInBinary, Integer.toBinaryString(actual));
+  }
+
+  @ParameterizedTest(name = "input: {0}, i: {1}, val: {2}")
+  @CsvSource({"10,0,1,1011", "10,0,0,1010", "10,1,0,1000", "10,1,1,1010"})
+  void updateIthBitTest(int num, int i, int bit, String expectedInBinary) {
+    int actual = BitUtils.updateIthBit(num, i, bit);
+    Assertions.assertEquals(expectedInBinary, Integer.toBinaryString(actual));
+  }
+
+  @ParameterizedTest(name = "input: {0}, i: {1}")
+  @CsvSource({"15,1,1110", "15,2,1100", "15,3,1000", "15,4,0"})
+  void clearLastIBitsTest(int num, int i, String expectedInBinary) {
+    int actual = BitUtils.clearLastIbits(num, i);
+    Assertions.assertEquals(expectedInBinary, Integer.toBinaryString(actual));
+  }
+
+  @ParameterizedTest(name = "input: {0}, i: {1}, j: {2}")
+  @CsvSource({"15,0,1,1100", "15,2,3,11", "15,1,3,1", "15,0,3,0"})
+  void clearBitsInRangeTest(int num, int i, int j, String expectedInBinary) {
+    int actual = BitUtils.clearBitsInRange(num, i, j);
+    Assertions.assertEquals(expectedInBinary, Integer.toBinaryString(actual));
+  }
+
+  @ParameterizedTest(name = "input: {0}, i: {1}, j: {2}")
+  @CsvSource({"15,0,1,0,1100", "15,1,2,0,1001"})
+  void replaceBitsInRangeTest(int num, int i, int j, int val, String expectedInBinary) {
+    int actual = BitUtils.replaceBitsInRange(num, i, j, val);
+    System.out.println("Integer.toBinaryString(actual) = " + Integer.toBinaryString(actual));
     Assertions.assertEquals(expectedInBinary, Integer.toBinaryString(actual));
   }
 }
